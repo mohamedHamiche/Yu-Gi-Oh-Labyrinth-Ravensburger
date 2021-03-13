fbb61342ba1554a908698a0da4173fb9b67260f7
Ahmed YAYA <a7medrd99@gmail.com>
2021-03-13 15:04:48 +0000

    Update lirePseudo.c

--- Implémentation /test unitaires/lire_pseudo.c
+++ Implémentation /test unitaires/lire_pseudo.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *lire_pseudo(unsigned int numJoueur);
+char *lirePseudo(unsigned int numJoueur);
 int main()
 {
 
-	char *pseudo =lire_pseudo(1);
+	char *pseudo =lirePseudo(1);
 	printf("%s--\n",pseudo );
 	free(pseudo);
 	return 0;
 }
 
-char *lire_pseudo(unsigned int numJoueur)
+char *lirePseudo(unsigned int numJoueur)
 {
 	char *pseudo= malloc(25*sizeof(char));
 	printf("Donnez le pseudo du joueur %d :\n",numJoueur);
